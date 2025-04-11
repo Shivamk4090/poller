d38fb17fca56b6a88ea3e88ebfd86f2a02d749dc
Shivam Kumar <shivam.fot409@gmail.com>
2025-04-12 00:09:08 +0530

    db name changed

--- poller/settings.py
+++ poller/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'poller.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'poll.sqlite3',
     }
 }
 
