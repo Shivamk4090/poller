3ed1d8817bf9506a4c78a3310cb98a2ae888cbd6
Shivam Kumar <shivam.fot409@gmail.com>
2025-04-11 21:04:26 +0530

    login-sign up styling

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+
+class StyledLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': field.label,
+            })
+
+class StyledRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': field.label
+            })
--- core/views.py
+++ core/views.py
@@ -3,18 +3,19 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from .forms import StyledLoginForm, StyledRegisterForm
 
 # Create your views here.
 
 class Login(LoginView):
-    form_class = AuthenticationForm
+    form_class = StyledLoginForm
     template_name =  "auth/login.html"
     redirect_authenticated_user = True
     
     
 
 class SignUp(CreateView):
-    form_class = UserCreationForm
+    form_class = StyledRegisterForm
     template_name =  "auth/signup.html"
     success_url = reverse_lazy('sign_in')
 
